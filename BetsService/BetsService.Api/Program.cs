f48200e271f7892aa1f2962738c6132da85e0655
Skorodumov Michaile <m300881@mail.ru>
2024-10-26 11:19:25 +0300

    added first migration for bets service

--- BetsService/BetsService.Api/Program.cs
+++ BetsService/BetsService.Api/Program.cs
@@ -1,8 +1,18 @@
+using BetsService.Services.Helpers;
+using BetsService.Api.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var config = builder.Configuration;
+
+builder.Services
+    .AddBetsServices()
+    .AddServices(config);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ BetsService/BetsService.DataAccess/Migrations/20241026081253_CreateDb.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BetsService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    EventStartTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    BetsEndTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsOver = table.Column<bool>(type: "boolean", nullable: false),
+                    IsCanceled = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventOutcomes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    CurrentOdd = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsHappened = table.Column<bool>(type: "boolean", nullable: true),
+                    BetsClosed = table.Column<bool>(type: "boolean", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventOutcomes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventOutcomes_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventOutcomes_EventId",
+                table: "EventOutcomes",
+                column: "EventId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventOutcomes");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
