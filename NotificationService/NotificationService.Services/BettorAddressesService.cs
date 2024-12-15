f0f5e962f0c288d285e2a34adf455449426fec77
Skorodumov Michaile <m300881@mail.ru>
2024-12-15 11:51:12 +0300

    used MappingProfile in unit tests

--- NotificationService/NotificationService.Services/BettorAddressesService.cs
+++ NotificationService/NotificationService.Services/BettorAddressesService.cs
@@ -66,7 +66,7 @@ namespace NotificationService.Services
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"[BettorAddressesService][GetBettorAddressesAsync] Exception: {ex.ToString()}");
-                throw new Exception(ex.ToString());
+                throw; // new Exception(ex.ToString());
             }
         }
 
--- NotificationService/NotificationService.Tests/Services/BettorAddressesServiceTests.cs
+++ NotificationService/NotificationService.Tests/Services/BettorAddressesServiceTests.cs
@@ -5,7 +5,10 @@ using NotificationService.DataAccess.Repositories;
 using NotificationService.Services;
 using FluentAssertions;
 using NotificationService.Domain.Directories;
-using Bets.Abstractions.Domain.Repositories.Interfaces;
+using NotificationService.Services.Exceptions;
+using NotificationService.Models;
+using AutoMapper;
+using NotificationService.Services.Helpers;
 
 namespace NotificationService.Tests.Services
 {
@@ -17,7 +20,15 @@ namespace NotificationService.Tests.Services
         public BettorAddressesServiceTests()
         {
             var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
             _bettorAddressRepositoryMock = fixture.Freeze<Mock<IBettorAddressRepository>>();
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            fixture.Register<IMapper>(() => new Mapper(mapperConfig));
+
             _bettorAddressesService = fixture.Build<BettorAddressesService>().OmitAutoProperties().Create();
         }
 
@@ -28,11 +39,67 @@ namespace NotificationService.Tests.Services
             var request = TestHelper.CreateBaseBettorAddressesRequest();
 
             // Act
-            var result = await _bettorAddressesService.AddBettorAddressesAsync(request, CancellationToken.None);
+            _ = await _bettorAddressesService.AddBettorAddressesAsync(request, CancellationToken.None);
 
             // Assert
             _bettorAddressRepositoryMock
                 .Verify(repo => repo.AddAsync(It.IsAny<BettorAddresses>()), Times.Once);
+        }
+
+        [Fact]
+        public async void GetBettorAddressesAsync_AddressNotFound_NotFoundException()
+        {
+            // Arrange
+            var addressId = Guid.Parse("def47943-7aaf-44a1-ae21-05aa4948b165");
+            BettorAddresses address = null;
+
+            _bettorAddressRepositoryMock.Setup(repo => repo.GetByIdAsync(addressId))
+                .ReturnsAsync(address);
+
+            // Act
+            var act = async () => await _bettorAddressesService.GetBettorAddressesAsync(addressId);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<NotFoundException>(act);
+            Assert.Equal($"Адрес с идентификатором {addressId} не найден.", exception.Message);
+        }
+
+        [Fact]
+        public async void GetBettorAddressesAsync_PassedSuccess_BettorAddressResponse()
+        {
+            // Arrange
+            BettorAddresses address = TestHelper.CreateBettorAddresses();
+            var addressId = address.Id;
+
+            var expectedResult = new BettorAddressResponse()
+            {
+                Id = addressId,
+                Address = address.Address,
+                Priority = address.Priority,
+                Messenger = new MessengerResponse()
+                {
+                    Id = address.Messenger.Id,
+                    Name = address.Messenger.Name,
+                },
+                Bettor = new BettorResponse()
+                {
+                    Id = address.Bettor.Id,
+                    Nickname = address.Bettor.Nickname,
+                }
+            };
+
+            _bettorAddressRepositoryMock.Setup(repo => repo.GetByIdAsync(addressId))
+                .ReturnsAsync(address);
+
+            // Act
+            var result = await _bettorAddressesService.GetBettorAddressesAsync(addressId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<BettorAddressResponse>()
+                .Which.Should().BeEquivalentTo(expectedResult);
+            _bettorAddressRepositoryMock
+                .Verify(repo => repo.GetByIdAsync(addressId), Times.Once);
         }
     }
 }
