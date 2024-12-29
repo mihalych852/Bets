12a43686cce18944c07fa6547d9ed05fa68195d3
Kralichka <88140274+Kralichka@users.noreply.github.com>
2024-12-29 17:48:00 +0300

    Update IndexBets.tsx

--- client/src/pages/IndexBets.tsx
+++ client/src/pages/IndexBets.tsx
@@ -17,65 +17,7 @@ export default function IndexBets(){
                 setData(response.data);
                 
             })
-    }, [])
-    const testList: eventDTO[] = [
-        {
-            id:"b314e08e-5e6c-4215-b8df-506e884960f7",
-            description: "Реал Мадрид - Барселона",
-            betsEndTime: new Date("13-12-2024"),
-            eventStartTime: new Date("11-12-2024"),
-            eventOutcomes: [{
-                id: "b314e08e-5e6c-4215-b8df-506e884960f8",
-                description: "outcome 1",
-                currentOdd: 2
-            },{
-                id:"b314e08e-5e6c-4215-b8df-506e884960f9",
-                description: "outcome 2",
-                currentOdd: 4
-            },
-        ]
-        },
-        {
-            id:"2",
-            description: "Победа в выборах",
-            betsEndTime: new Date("13-12-2024"),
-            eventStartTime: new Date("11-12-2024"),
-            eventOutcomes: [{
-                id:"3",
-                description: "Трапм",
-                currentOdd: 2
-            },{
-                id:"4",
-                description: "Кто-то там еще",
-                currentOdd: 4
-            },{
-                id:"456456",
-                description: "Кто-то там еще",
-                currentOdd: 4
-            },{
-                id:"786753",
-                description: "Кто-то там еще",
-                currentOdd: 4
-            },
-        ]
-        },
-        {
-            id:"3",
-            description: "Реал Мадрид - Барселона",
-            betsEndTime: new Date("13-12-2024"),
-            eventStartTime: new Date("11-12-2024"),
-            eventOutcomes: [{
-                id: "1",
-                description: "outcome 1",
-                currentOdd: 2
-            },{
-                id:"2",
-                description: "outcome 2",
-                currentOdd: 4
-            },
-        ]
-        }
-    ]
+    }, [])    
     return(
     <>
         <EventList events={data}/>
